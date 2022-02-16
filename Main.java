7d7da94bcea2413f33d3e63a2f842d610dd2b782
abhi-10001 <abhicse10001@gmail.com>
2022-02-16 19:18:45 +0530

    Adding one extra file

---
+++ Main.java
@@ -0,0 +1,30 @@
+//Guess the number.
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //generating a random number
+        System.out.print("Guess the number: ");
+        double randomNumber=Math.random();
+        randomNumber = randomNumber * 100;
+        //randomNumber = randomNumber +1;
+        int randomInt =(int) randomNumber;
+        int x = scan.nextInt();
+        int count=0;
+        while (x != randomInt) {
+
+            if (x < randomInt) {
+                System.out.println(x + " is small");
+            } else {
+                System.out.println(x + " is big");
+            }
+           x = scan.nextInt();
+            count++;
+        }
+        System.out.println("Bingo!, You guessed it.\nThe number is " + randomInt);
+        System.out.println("You guess the number in " + count + " tries.");
+
+    }
+}
